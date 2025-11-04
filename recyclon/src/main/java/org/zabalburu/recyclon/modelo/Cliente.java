59be84a47c732f263b498d04fc5cdb6a08908992
mtxutxi <jesusalejandro.martinez.gonzalez@zabalburu.org>
2025-11-04 23:00:31 +0100

    Clases con campos y relaciones
    Clases con campos y relaciones (Clase Cliente creada)

--- recyclon/src/main/java/org/zabalburu/recyclon/modelo/Cliente.java
+++ recyclon/src/main/java/org/zabalburu/recyclon/modelo/Cliente.java
@@ -1,7 +1,6 @@
 package org.zabalburu.recyclon.modelo;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 
 import jakarta.persistence.Column;
@@ -9,18 +8,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Entity //Marcar como entidad JPA -> Aplicar Serializable
-@Table(name = "pedido") //Nombre de la tabla en la BBDD
+@Table(name = "cliente") //Nombre de la tabla en la BBDD
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Pedido implements Serializable {
+public class Cliente implements Serializable {
 	
 	/**
 	 * 
@@ -28,20 +25,26 @@ public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L; //Para que coincida el id de version incluso despues de agregar más campos a la clase
 	
 	
-	@Id //Para marcar "id_pedido" como PrimaryKey
+	@Id //Para marcar "id_cliente" como PrimaryKey
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //Para indicar que el id lo genera la BBDD
 	@Column(
-			name = "id_pedido" //Nombre de la columna en la BBDD
+			name = "id_cliente" //Nombre de la columna en la BBDD
 	)
 	@EqualsAndHashCode.Include
-	private Integer idPedido;
+	private Integer idCliente;
 	
-	private Date fecha;
+	@Column(
+			columnDefinition = "varchar(100)" //Para asignar limite de 100 caracteres
+		)
+	private String nombre;
 	
-	@ManyToOne //N a 1
-	@JoinColumn(name = "id_cliente") //Apuntamos al id de Cliente (ForeignKey)
-	private Cliente cliente;
+	private String apellidos;
 	
-	@OneToMany(mappedBy = "pedido") //Relacion 1 a N, mappedBy -> apuntamos a la relacion de LineaPedido
-	private List<LineaPedido> lineasPedido;
+	@OneToMany(mappedBy = "cliente") //Relacion 1 a N, mappedBy -> apuntamos a la relacion de Pedido
+	private List<Pedido> pedidos;
+
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", nombre=" + nombre + ", apellidos=" + apellidos + "]";
+	}
 }
