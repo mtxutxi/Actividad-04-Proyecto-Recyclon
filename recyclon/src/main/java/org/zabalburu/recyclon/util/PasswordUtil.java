5754455c3da2a4c80e2c8525a512ac4bf7f04a49
belenDevz <florenciabelen.barbas.bogado@zabalburu.org>
2025-11-11 12:48:24 +0100

    bcrypt + pom.xml

--- recyclon/src/main/java/org/zabalburu/recyclon/util/PasswordUtil.java
+++ recyclon/src/main/java/org/zabalburu/recyclon/util/PasswordUtil.java
@@ -1,5 +1,21 @@
 package org.zabalburu.recyclon.util;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 public class PasswordUtil {
 
+    public static String hashPassword(String password) {
+        return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    }
+
+    public static boolean verifyPassword(String password, String hashedPassword) {
+        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), hashedPassword);
+        return result.verified;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PasswordUtil.hashPassword("passw0rd"));
+        System.out.println(PasswordUtil.verifyPassword("passw0rd", "$2a$12$x7Ea5/2qRDvRy3Q9e.Oj2OmcXUFnK6BO8yQL3FEGcwu5Wbb5BcUMK"));
+
+    }
 }
